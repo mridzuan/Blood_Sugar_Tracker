d16aa036823b7bd4d8cb88460f8d5467bb64a222
milk <jcapps@protonmail.com>
2019-12-27 19:58:51 -0600

    4

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-const proxy = require('http-proxy-middleware')
 
-module.exports = function(app) {
-    // add other server routes to path array
-    app.use(proxy(['/' ], { target: 'http://localhost:5000' }));
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
