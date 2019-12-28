04837dac7704dc4bdb70cd780a0299d9b6cb1af3
milk <jcapps@protonmail.com>
2019-12-27 20:43:34 -0600

    Adjust store to work on all browsers.

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const initialState = {};
+/*const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
@@ -12,6 +12,25 @@ const store = createStore(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
+);*/
+
+
+const initialState = {};
+
+
+const middleware = [thunk];
+
+
+const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
+
+
+const store = createStore(
+rootReducer,
+initialState,
+enhancer
 );
 
 
