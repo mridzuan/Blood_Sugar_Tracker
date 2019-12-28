8b84e5e9d100bc864586af0b7d402373e8b30f57
milk <jcapps@protonmail.com>
2019-12-27 20:08:15 -0600

    5

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,5 +2,10 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
     // add other server routes to path array
-    app.use(proxy(['/' ], { target: 'http://localhost:5000' }));
+    app.use(proxy(['/bloodsugar' ], { target: 'http://localhost:5000' }));
+    app.use(proxy(['/forgotpassword' ], { target: 'http://localhost:5000' }));
+    app.use(proxy(['/login' ], { target: 'http://localhost:5000' }));
+    app.use(proxy(['/resetpassword' ], { target: 'http://localhost:5000' }));
+    app.use(proxy(['/updatePasswordViaEmail' ], { target: 'http://localhost:5000' }));
+    app.use(proxy(['/users' ], { target: 'http://localhost:5000' }));
 }
